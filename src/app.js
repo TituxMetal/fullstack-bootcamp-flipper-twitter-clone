8be10c7e4b6ef7bc21222c36af90950d4b06f32c
Titux Metal <github@lgdweb.fr>
2021-06-02 19:26:20 +0200

    Updating app.js file; uses session store and error handler

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ import express from 'express'
 import path from 'path'
 
 import { port } from '#root/config'
+import { errorHandler } from '#root/middlewares'
 import { mainRoutes } from '#root/routes'
 
-const createApp = () => {
+const createApp = sessionStore => {
   const app = express()
 
   app.use(express.static('public'))
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
+  app.use(sessionStore)
 
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'pug')
@@ -19,10 +21,12 @@ const createApp = () => {
   app.all('*', async (req, res, next) => {
     const error = `${req.path} page does not exists`
 
-    res.status(404).render('error', { error })
+    res.status(404).render('error', { message: error })
 
     return next()
   })
+
+  app.use(errorHandler)
 
   const server = app.listen(port, '0.0.0.0', () =>
     console.info(`Server is listening on http://localhost:${port}`)
