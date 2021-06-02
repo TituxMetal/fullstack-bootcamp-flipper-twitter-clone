48a8c2fd4d7d815b1099eef3dde082742517475b
Titux Metal <github@lgdweb.fr>
2021-06-02 19:14:33 +0200

    Updating index.js file in config; adds sessionOptions

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,26 @@
 const {
   DATABASE_URI = 'redis://username:password@localhost:port',
   NODE_ENV = 'development',
-  PORT = 3000
+  PORT = 3000,
+  SESSION_IDLE_TIMEOUT = 360000,
+  SESSION_NAME = 'sid',
+  SESSION_SECRET = 'please keep this secret'
 } = process.env
 
 export const databaseUri = DATABASE_URI
 
 export const inProd = NODE_ENV === 'production'
 
-export const port = PORT
+export const port = +PORT
+
+export const sessionOptions = {
+  cookie: {
+    maxAge: +SESSION_IDLE_TIMEOUT,
+    sameSite: true,
+    secure: inProd
+  },
+  name: SESSION_NAME,
+  resave: true,
+  saveUninitialized: true,
+  secret: SESSION_SECRET
+}
