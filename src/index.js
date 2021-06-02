c4ed9d49b4b0dbb740f804c0603c395fd5fc5ec6
Titux Metal <github@lgdweb.fr>
2021-06-02 19:22:45 +0200

    Updating index.js file; adds redis store for session and pass it to createApp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+import connectRedis from 'connect-redis'
+import session from 'express-session'
+
+import { sessionOptions } from '#root/config'
+import { redisClient } from '#root/database'
 import { shutdownServer } from '#root/helpers'
 
 import createApp from './app'
 
-const server = createApp()
-
-// @TODO connection to redis
+const RedisStore = connectRedis(session)
+const store = new RedisStore({ client: redisClient })
+const sessionStore = session({
+  ...sessionOptions,
+  store
+})
+const server = createApp(sessionStore)
 
 shutdownServer(server)
