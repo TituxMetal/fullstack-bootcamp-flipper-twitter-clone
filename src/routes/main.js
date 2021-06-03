851609ea5275334c96476257659389473176f802
Titux Metal <github@lgdweb.fr>
2021-06-03 21:58:01 +0200

    Updating main.js file in routes; finishes all the route handlers

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,16 +1,78 @@
 import { Router } from 'express'
 
-import { loginOrSignup } from '#root/utils'
+import { redisClient } from '#root/database'
+import {
+  getAllPosts,
+  handleAddPost,
+  handleFollowUser,
+  getFollowingUsers,
+  loginOrSignup
+} from '#root/utils'
 
 const router = new Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   if (!req.session.userid) {
     return res.render('login')
   }
-  console.log(req.session)
 
-  return res.render('dashboard')
+  const currentUser = await redisClient.hget(
+    `user:${req.session.userid}`,
+    'email'
+  )
+  console.log('currentUser', currentUser)
+
+  const users = await getFollowingUsers(currentUser)
+  console.log('users', users)
+
+  const timeline = await getAllPosts(currentUser)
+  console.log('timeline', timeline)
+
+  return res.render('dashboard', { currentUser, timeline, users })
+})
+
+router.get('/post', (req, res) => {
+  if (!req.session.userid) {
+    return res.render('login')
+  }
+
+  return res.render('post')
+})
+
+router.post('/post', async (req, res) => {
+  if (!req.session.userid) {
+    return res.render('login')
+  }
+
+  const { message } = req.body
+
+  const userId = req.session.userid
+
+  const currentUser = await redisClient.hget(`user:${userId}`, 'email')
+
+  const postId = await handleAddPost(userId, currentUser, message)
+
+  const followers = await redisClient.smembers(`followers:${currentUser}`)
+  console.log('message', message)
+  console.log('userId', userId)
+  console.log('currentUserEmail', currentUser)
+  console.log('postId returned by handleAddPost', postId)
+  console.log('followers', followers)
+
+  for (const follower of followers) {
+    redisClient.lpush(`timeline:${follower}`, postId)
+  }
+
+  return res.redirect('/')
+})
+
+router.post('/follow', async (req, res) => {
+  const userId = req.session.userid
+  const userToFollow = req.body.username
+
+  await handleFollowUser(userId, userToFollow)
+
+  return res.redirect('/')
 })
 
 router.post('/', async (req, res) => {
