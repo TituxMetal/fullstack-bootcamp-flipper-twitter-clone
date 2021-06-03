202392737f76002803dbde8d2a5bc14e3e1d8fc4
Titux Metal <github@lgdweb.fr>
2021-06-03 21:54:25 +0200

    Updating index.js file in utils; adds functions for adding and adding post and following other users

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,80 @@
 import { hash, compare, genSalt } from 'bcryptjs'
+import { formatDistance } from 'date-fns'
 
 import { redisClient } from '#root/database'
+
+export const getPostByKey = async (postId, key) =>
+  redisClient.hget(`post:${postId}`, key)
+
+export const getUserByEmail = async userId =>
+  redisClient.hget(`user:${userId}`, 'email')
+
+export const getFollowingUsers = async currentUser => {
+  console.log(currentUser)
+  const following = await redisClient.smembers(`following:${currentUser}`)
+  console.log('following', following)
+
+  const allUsers = await redisClient.hkeys('users')
+  // const promises = allUsers.map(async user => user)
+  // const results = await Promise.all(promises)
+  console.log('allUsers', allUsers)
+
+  const users = allUsers.filter(
+    user => user !== currentUser && following.indexOf(user) === -1
+  )
+
+  return users
+}
+
+export const handleFollowUser = async (userId, userToFollow) => {
+  const currentUser = await getUserByEmail(userId, 'email')
+
+  await redisClient.sadd(`following:${currentUser}`, userToFollow)
+  await redisClient.sadd(`followers:${userToFollow}`, currentUser)
+}
+
+export const getAllPosts = async currentUser => {
+  const allPosts = await redisClient.lrange(`timeline:${currentUser}`, 0, 100)
+
+  // https://zellwk.com/blog/async-await-in-loops/
+
+  const promises = allPosts.map(async post => {
+    const username = await getPostByKey(post, 'username')
+    const author = username.split('@')[0]
+    const message = await getPostByKey(post, 'message')
+    const timestamp = await getPostByKey(post, 'timestamp')
+    const timeString = formatDistance(
+      new Date(),
+      new Date(parseInt(timestamp, 10))
+    )
+
+    return { author, message, timeString }
+  })
+
+  const results = await Promise.all(promises)
+
+  return results
+}
+
+export const handleAddPost = async (userId, currentUserName, message) => {
+  const postId = await redisClient.incr('postid')
+  console.log('postId', postId)
+
+  await redisClient.hset(
+    `post:${postId}`,
+    'userid',
+    userId,
+    'username',
+    currentUserName,
+    'message',
+    message,
+    'timestamp',
+    Date.now()
+  )
+  await redisClient.lpush(`timeline:${currentUserName}`, postId)
+
+  return postId
+}
 
 export const handleSignup = async (email, password) => {
   const userid = await redisClient.incr('userid')
